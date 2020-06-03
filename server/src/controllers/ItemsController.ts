9114a259f852f7e9a4837c0fb4c6d0ee8c4a2122
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2020-06-02 21:27:24 -0300

    :sparkles: Criando backend da aplicação

---
+++ server/src/controllers/ItemsController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class ItemsController {
+    async index(request: Request, response: Response) {
+        const items = await knex('items').select('*');
+    
+        const serilializedItems = items.map(item => {
+            return {
+                id: item.id,
+                title: item.title,
+                image_url: `http://localhost:3333/uploads/${item.image}`,
+            };
+        })
+    
+        return response.json(serilializedItems);
+    }
+}
+
+export default ItemsController;
