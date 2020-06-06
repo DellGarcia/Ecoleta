bafcc3a93a3acfd2337dd5efeb7501136cad52bf
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2020-06-06 14:05:06 -0300

    :sparkles: Adicionando upload de imagens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,17 +1,22 @@
 import express from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 const pointController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', pointController.create);
 routes.get('/points', pointController.index);
 routes.get('/points/:id', pointController.show);
+
+routes.post('/points', upload.single('image'), pointController.create);
 
 export default routes;
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
+import Dropzone from '../../components/Dropzone';
+
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
  
@@ -29,6 +31,8 @@ interface IBGECityResponse {
 const CreatePoint = () => {
 
     const history = useHistory();
+
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const [items, setItems] = useState<Item[]>([]);
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
@@ -129,16 +133,20 @@ const CreatePoint = () => {
         const [ latitude, longitude ] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        };
+        const data = new FormData();
+
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+        
+        if(selectedFile)
+            data.append('image', selectedFile);
+
 
         await api.post('points', data);
 
@@ -158,8 +166,11 @@ const CreatePoint = () => {
                 </Link>
             </header>
 
+
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do ponto de coleta</h1>
+
+                <Dropzone onFileUploaded={setSelectedFile} />
 
                 <fieldset>
                     <legend>
